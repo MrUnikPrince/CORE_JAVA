e0b82b0a595fbed61ed4b1bb14c6e3db57e79825
MrUnikPrince <opuniqueprince@gmail.com>
2026-01-14 13:31:06 +0530

    A Simple Method for ATM PIN DIGIT counter --  QUESTION  added

--- AtmPinDigitCounter.java
+++ AtmPinDigitCounter.java
@@ -9,3 +9,53 @@ void main(){
     }
     IO.println("Number of digits: "+ count);
 }
+
+/**
+ * ATM PIN Digit Counter
+    * Description
+    * A bank’s ATM software needs to verify how many digits a customer’s PIN contains before processing it.
+
+Class Name
+-----------------
+PinValidator
+
+Method Details
+--------------------
+Method Name: countDigits
+Parameter: int number
+Return Type: void
+Variable Used:
+digitCount -> stores the number of digits
+
+Requirement :
+
+Use a loop to repeatedly divide the number by 10
+Increment digitCount on each division
+Stop when the number becomes 0
+Print the final digit count
+Examples
+Example 1:
+
+Input
+
+Enter your ATM PIN: 1353
+Output
+
+Number of digits: 4
+Example 2:
+
+Input
+
+Enter your ATM PIN: 12456
+Output
+
+Number of digits: 5
+Explanation
+
+Count digits by dividing number by 10 repeatedly
+Constraints
+The PIN number is a positive integer.
+The number of digits can vary (typically 4–6 for most ATMs).
+You must use a variable digitCount to store the number of digits.
+Use loops or mathematical operations to calculate the number of digits
+ */
